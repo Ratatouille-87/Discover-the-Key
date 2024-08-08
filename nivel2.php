ac40c401584c0470b6f6d60a9ce68e7275c51d52
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2024-08-08 16:49:13 -0300

    Update nivel2.php

--- nivel2.php
+++ nivel2.php
@@ -10,19 +10,19 @@
         <h1>ycvej?x=ULGgq1YoeXy</h1>
         <form method="POST" action="verificar.php">
             <input type="text" name="resposta" placeholder="Digite a resposta">
-            <button type="submit">Confirmar</button>
+            <button type="submit" name="submit">Confirmar</button>
         </form>
         <?php
-    if (isset($_POST['submit'])) {
-        $resposta = $_POST['resposta'];
-        if (strtolower($resposta) === 'Esses RECORDES OLÍMPICOS nunca vão ser quebrados') {
-            header("Location: vitoria.php");
-            exit();
-        } else {
-            echo "<p>Boa ideia, mas a resposta não é essa, tente novamente</p>";
+        if (isset($_POST['submit'])) {
+            $resposta = $_POST['resposta'];
+            if (strtolower($resposta) === 'Esses RECORDES OLÍMPICOS nunca vão ser quebrados') {
+                header("Location: vitoria.php");
+                exit();
+            } else {
+                echo "<p>Boa ideia, mas a resposta não é essa, tente novamente</p>";
+            }
         }
-    }
-    ?>
+        ?>
         <img src="https://aventurasnahistoria.com.br/media/uploads/1c442ae541.jpg" alt="Imperador César">
     </div>
 </body>
