00c5b82a92d3f9729230f8efc1b19ff70994f9c1
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2024-08-08 16:49:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
         <h1>Level 1</h1>
         <form method="POST" action="verificar.php">
             <input type="text" name="resposta" placeholder="Digite a resposta">
-            <button type="submit">Confirmar</button>
+            <button type="submit" name="submit">Confirmar</button>
         </form>
         <?php
-    if (isset($_POST['submit'])) {
-        $resposta = $_POST['resposta'];
-        if (strtolower($resposta) === 'Level 1') {
-            header("Location: nivel2.php");
-            exit();
-        } else {
-            echo "<p>Boa ideia, mas a resposta não é essa, tente novamente</p>";
+        if (isset($_POST['submit'])) {
+            $resposta = $_POST['resposta'];
+            if (strtolower($resposta) === 'Level 1') {
+                header("Location: nivel2.php");
+                exit();
+            } else {
+                echo "<p>Boa ideia, mas a resposta não é essa, tente novamente</p>";
+            }
         }
-    }
-    ?>
+        ?>
         <p class="frase-binaria">01001100 01100101 01110110 01100101 01101100 00100000 00110001</p>
     </div>
 </body>
