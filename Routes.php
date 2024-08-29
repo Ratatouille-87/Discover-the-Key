454d0affe5f9692238bd17994acd20a0aa9da12f
Bianca Brizola Viana <138807743+starbih@users.noreply.github.com>
2024-08-28 21:34:51 -0300

    Create Routes.php

---
+++ Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('nivel1', 'GameController::nivel1');
+$routes->get('nivel2', 'GameController::nivel2');
+$routes->get('vitoria', 'GameController::vitoria');
+$routes->get('test_connection', 'GameController::test_connection');
+$routes->post('verificar', 'GameController::verificar');
+$routes->post('nivel2', 'GameController::nivel2');
+
+
